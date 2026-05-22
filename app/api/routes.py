b705d0406c57d727316a73ead80ecbf87ccdec5f
Michael Kolmogorov <KMihal@yandex.ru>
2026-05-22 15:12:38 +0300

    [add] отладка пришедшего тензора log info

--- app/api/routes.py
+++ app/api/routes.py
@@ -29,7 +29,7 @@ async def recognize(item: Request) -> Response:
     task_id = str(uuid.uuid4())
 
     # отладка: сохраняем пришедший большой тензор в лог, чтобы он полностью отображался
-    logger.debug(f"Received tensor for task {task_id}: {json.dumps(item.tensor)})")
+    logger.info(f"Received tensor for task {task_id}: {json.dumps(item.tensor)})")
 
     try:
         message_id = redis_client.xadd(
